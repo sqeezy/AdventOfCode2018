1ed27acd0704081233ed53b65e508dd122e6b9cf
Anton Herzog <info@antonherzog.com>
2018-12-04 08:40:24 +0100

    Solve Day01

--- Day01/Program.fs
+++ Day01/Program.fs
@@ -1,8 +1,21 @@
-﻿// Learn more about F# at http://fsharp.org
+﻿open System
 
-open System
+let (|Prefix|_|) (p:string) (s:string) =
+    if s.StartsWith(p) then
+        Some(s.Substring(p.Length))
+    else
+        None
+
+
+let addLineToSum sum line =
+    match line with
+    | Prefix "-" rest -> sum - (rest |> int)
+    | Prefix "+" rest -> sum + (rest |> int) 
+    | _               -> sum
+
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    let result = dataLines |> Seq.fold addLineToSum 0
+    printfn "%i" result
     0 // return an integer exit code
