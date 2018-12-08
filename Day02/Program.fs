888459d297c84e187b9553e873521872bdadb928
Anton Herzog <info@antonherzog.com>
2018-12-08 18:27:07 +0100

    Solve Day02/1

---
+++ Day02/Program.fs
@@ -0,0 +1,23 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let containsTwoThree (s:string) = 
+    let countOfChars = s.ToCharArray()
+                        |> Seq.groupBy (id)
+                        |> Seq.map (fun (_, cs)-> cs |> Seq.length)
+                        |> Seq.distinct
+    let containsTwo = countOfChars |> Seq.contains 2
+    let containsThree = countOfChars |> Seq.contains 3
+    containsTwo, containsThree
+
+let solvePartOne list = 
+    let twosAndThreesPresentPerLine = list |> Seq.map containsTwoThree
+    let twos = twosAndThreesPresentPerLine |> Seq.filter (fun (b, _) -> b) |> Seq.length
+    let threes = twosAndThreesPresentPerLine |> Seq.filter (fun (_, b) -> b) |> Seq.length
+    twos * threes
+
+[<EntryPoint>]
+let main argv =
+    printfn "Part One Result: %d" (solvePartOne Input.dataLines)
+    0 // return an integer exit code
