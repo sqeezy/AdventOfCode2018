ac9289dfa3b9a5653672b41f57653cf769b80581
Anton Herzog <info@antonherzog.com>
2018-12-09 13:27:03 +0100

    Solve Day02/2

--- Day02/Program.fs
+++ Day02/Program.fs
@@ -10,7 +10,6 @@ let containsTwoThree (s:string) =
     let containsTwo = countOfChars |> Seq.contains 2
     let containsThree = countOfChars |> Seq.contains 3
     containsTwo, containsThree
-
 let solvePartOne list = 
     let twosAndThreesPresentPerLine = list |> Seq.map containsTwoThree
     let twos = twosAndThreesPresentPerLine |> Seq.filter (fun (b, _) -> b) |> Seq.length
@@ -21,23 +20,31 @@ let cartesianProduct n g = List.map (fun (n,g)->[n;g]) (List.allPairs n g)
 
 let differentByOne (a, b) = 
     let compareElementwise = Seq.zip a b 
-                              |> Seq.map (fun (a,b)-> if a=b then None else Some (a,b))
+                              |> Seq.map (fun (a,b)-> if a=b then Some a else None)
 
     let differentElements = compareElementwise
-                                  |> Seq.filter(Option.isSome)
+                                  |> Seq.filter(Option.isNone)
+    let sameElements = compareElementwise
+                          |> Seq.filter Option.isSome
+                          |> Seq.map    Option.get
+
     let numOfDifferentElements = differentElements |> Seq.length
 
     match numOfDifferentElements with
-    | 1 -> Some (Seq.head differentElements)
+    | 1 -> Some sameElements
     | _ -> None
 
 let solvePartTwo (list : string list) = cartesianProduct list list
-                                            |> Seq.map (fun l -> (l.[0].ToCharArray(), l.[1].ToCharArray()))
-                                            |> Seq.map differentByOne
+                                            |> Seq.map    (fun l -> (l.[0].ToCharArray(), l.[1].ToCharArray()))
+                                            |> Seq.map    differentByOne
                                             |> Seq.filter Option.isSome
-
+                                            |> Seq.head 
+                                            |> Option.get 
+                                            |> Array.ofSeq 
+                                            |> System.String
 
 [<EntryPoint>]
 let main argv =
     printfn "Part One Result: %d" (solvePartOne Input.dataLines)
+    printfn "Part Two Result: %A" (solvePartTwo Input.dataLines )
     0 // return an integer exit code
